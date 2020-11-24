d2c43f763d5afd1e96e59f2da8d2381c8531f837
Paulo Gonçalves <paulorochag@hotmail.com>
2020-11-24 15:58:07 -0300

    ci(mutation test): executar no pull request o teste de mutação apenas dos arquivos alterados

--- test/stryker.conf.js
+++ test/stryker.conf.js
@@ -9,18 +9,13 @@ module.exports = {
     '!src/utils/ambiente.js'
   ],
   thresholds: {
-    high: 90,
-    low: 80,
-    break: 75
+    high: 95,
+    low: 85,
+    break: 80
   },
-  packageManager: 'npm',
   reporters: ['html', 'dashboard', 'clear-text', 'progress'],
-  timeoutMS: 8000,
   testRunner: 'command',
   commandRunner: {
-    command: 'cross-env NODE_ENV=serverest-test mocha --config test/.mocharc.js'
-  },
-  mochaOptions: {
-    config: './test/.mocharc.js'
+    command: 'mocha --config test/.mocharc.js'
   }
 }
