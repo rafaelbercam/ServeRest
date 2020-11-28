9ba9c2ce06a5e5aba26ff3ee4f98c120b33e50d8
Paulo Gonçalves <paulorochag@hotmail.com>
2020-11-28 17:43:28 -0300

    test: sanity parcial

--- test/utils/global.js
+++ test/utils/global.js
@@ -1,4 +1,5 @@
-const app = require('../../src/app')
 const supertest = require('supertest')
+
+const app = (process.env.TEST === 'sanity') ? 'http://localhost:3000' : require('../../src/app')
 
 global.request = supertest(app)
